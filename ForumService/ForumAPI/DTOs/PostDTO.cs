f19157efa7d3346a354468dd8f46bca30e4f386f
MaikeMee <113889971+MaikeMee@users.noreply.github.com>
2024-06-18 16:46:15 +0200

    Update PostDTO.cs

--- ForumService/ForumAPI/DTOs/PostDTO.cs
+++ ForumService/ForumAPI/DTOs/PostDTO.cs
@@ -7,13 +7,13 @@ namespace ForumAPI.DTOs
 {
     public class PostDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
-        public int AuthorId { get; set; }
+        public required int AuthorId { get; set; }
         public string Audience { get; set; } = string.Empty;
-        public DateTime DateTime { get; set; }
+        public required DateTime DateTime { get; set; }
         public string Url { get; set; } = string.Empty;
-        public int ForumId { get; set; }
+        public required int ForumId { get; set; }
     }
 }
