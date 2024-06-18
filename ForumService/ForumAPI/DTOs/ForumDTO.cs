0d4e359b23f356182b7a6090511c93c0eaefa49a
MaikeMee <113889971+MaikeMee@users.noreply.github.com>
2024-06-18 16:46:41 +0200

    Update ForumDTO.cs

--- ForumService/ForumAPI/DTOs/ForumDTO.cs
+++ ForumService/ForumAPI/DTOs/ForumDTO.cs
@@ -5,7 +5,7 @@ namespace ForumAPI.DTOs
 {
     public class ForumDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string CategoryName { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Audience { get; set; } = string.Empty;
