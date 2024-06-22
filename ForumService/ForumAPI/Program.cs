333c4d3ae2b8d25ac817f4234b60b2cbeed98e8e
Meek,Maike M.Z.E. <m.meek@student.fontys.nl>
2024-06-22 22:19:32 +0200

    x

--- ForumService/ForumAPI/Program.cs
+++ ForumService/ForumAPI/Program.cs
@@ -68,30 +68,7 @@ builder.Services.AddAuthentication(options =>
 });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new() { Title = "User Service API", Version = "v1" });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter into field the word 'Bearer' following by space and JWT",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            Array.Empty<string>()
-        }
-    });
-});
+
 builder.Services.AddDbContext<ForumContext>(options =>
 {
     options.UseNpgsql(builder.Configuration["ConnectionStrings:PostgreSqlConnection"]);
@@ -112,8 +89,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
